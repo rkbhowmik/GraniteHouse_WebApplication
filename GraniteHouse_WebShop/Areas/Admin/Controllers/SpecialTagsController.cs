67c786ac69858d016eb3eea923f9ac410a173181
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2018-12-16 22:41:08 +0100

    - Special tags controller with Index view created

---
+++ GraniteHouse_WebShop/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse_WebShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse_WebShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+    }
+}
