fc41645d3b7a71462aa7cacf134ac9aeb0520279
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2018-12-16 22:13:50 +0100

    - ProductTypes controller and Index view created

---
+++ GraniteHouse_WebShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse_WebShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse_WebShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        //database object gets by dependency injection
+        private readonly ApplicationDbContext _db;
+
+        // a connstructor needed for dependency injection
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+    }
+}
