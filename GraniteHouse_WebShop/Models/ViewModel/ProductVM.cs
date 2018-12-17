782f05a599859f1561e63984596a6f25a826c949
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2018-12-17 21:31:27 +0100

    -Product View Model created

---
+++ GraniteHouse_WebShop/Models/ViewModel/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse_WebShop.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public Products Products { get; set; }
+
+        public IEnumerable<ProductTypes> ProductTypes { get; set; }
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+
+    }
+}
