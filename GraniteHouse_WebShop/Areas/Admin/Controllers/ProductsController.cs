fabe586d42cf9a4161cb9d8e06302f5ef61adf32
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2018-12-17 22:44:33 +0100

    -Create action method

--- GraniteHouse_WebShop/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse_WebShop/Areas/Admin/Controllers/ProductsController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse_WebShop.Data;
 using GraniteHouse_WebShop.Models;
 using GraniteHouse_WebShop.Models.ViewModel;
+using GraniteHouse_WebShop.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,12 +18,15 @@ namespace GraniteHouse_WebShop.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ProductsVM ProductsVM { get; set; }
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
+
             ProductsVM = new ProductsVM()
             {
                 ProductTypes = _db.ProductTypes.ToList(),
@@ -33,5 +40,60 @@ namespace GraniteHouse_WebShop.Controllers
             var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
             return View(await products.ToListAsync());
         }
+
+        //GET Create Action Method
+
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+        //Post: Products Create Method
+        // By using ActionName("Create") we can use whatever name of the create method
+        [HttpPost,ActionName("Create")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> CreatePOST()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+
+            _db.Products.Add(ProductsVM.Products);
+             await _db.SaveChangesAsync();
+
+            // Image being saved
+            string webRootPath = _hostingEnvironment.WebRootPath;
+
+            //
+            var files = HttpContext.Request.Form.Files;
+
+            var productsFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+            if(files.Count!=0)
+            {
+                // Image has been uploaded
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+            }
+            else
+            {
+                // when user does not upload image
+                string uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".png");
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
