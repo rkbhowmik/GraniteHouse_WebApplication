12481d9e29511fd029ac71aa49a6c1ae821bd9a5
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2018-12-18 00:29:13 +0100

    - Dropdown list created

--- GraniteHouse_WebShop/Utility/SD.cs
+++ GraniteHouse_WebShop/Utility/SD.cs
@@ -8,6 +8,6 @@ namespace GraniteHouse_WebShop.Utility
     public class SD
     {
         public const string DefaultProductImage = "default_image.png";
-        public const string ImageFolder = @"image\ProductImage";
+        public const string ImageFolder = @"images\ProductImage";
     }
 }
